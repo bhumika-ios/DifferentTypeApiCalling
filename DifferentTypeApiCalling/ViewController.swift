d79137dd54905d6da6dfba5c46a588df9c7ad745
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-21 15:15:34 +0530

    create outlet and button in viewcontroller

--- DifferentTypeApiCalling/ViewController.swift
+++ DifferentTypeApiCalling/ViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblApi: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
+@IBAction func btnFirstApi(_ sender: UIButton) {
+//        let controller = storyboard?.instantiateViewController(withIdentifier: "FirstApiVc") as! FirstApiViewController
+//
+//        controller.modalPresentationStyle = .fullScreen
+//        present(controller, animated: true, completion: nil)
+}
 
 
 }
