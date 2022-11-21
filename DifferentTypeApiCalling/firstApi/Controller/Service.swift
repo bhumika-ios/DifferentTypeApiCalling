98120da4e060c648cd4569918bc91772909d1a32
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-21 15:31:23 +0530

    json restful api call for fetch data of api

--- DifferentTypeApiCalling/firstApi/Controller/Service.swift
+++ DifferentTypeApiCalling/firstApi/Controller/Service.swift
@@ -5,4 +5,36 @@
 //  Created by Bhumika Patel on 21/11/22.
 //
 
-import Foundation
+import UIKit
+
+class Service: NSObject {
+
+    
+    static let shareInstance = Service()
+    
+    func getAllMovieData(completion: @escaping([MovieModel]?, Error?) -> ()){
+        let urlString = "https://itunes.apple.com/search?media=music&term=bollywood"
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let err = error{
+                completion(nil,err)
+                print("Loading data error: \(err.localizedDescription)")
+            }else{
+                guard let data = data else { return }
+                do{
+                    var arrMovieData = [MovieModel]()
+                    let results = try JSONDecoder().decode(ResultsModel.self, from: data)
+                    for movie in results.results{
+                        arrMovieData.append(MovieModel(artistName: movie.artistName!, trackName: movie.trackName!))
+                    }
+                    completion(arrMovieData, nil)
+                }catch let jsonErr{
+                    print("json error : \(jsonErr.localizedDescription)")
+                }
+            }
+        }.resume()
+    }
+    
+    
+    
+}
