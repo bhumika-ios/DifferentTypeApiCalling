d3aaf900021f72f4c0c1ea1b5a697c57c6edd9d5
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-21 16:00:53 +0530

    userviewmodel create json api with alamofire package added

--- DifferentTypeApiCalling/SecondApi/Controller/UserViewModel.swift
+++ DifferentTypeApiCalling/SecondApi/Controller/UserViewModel.swift
@@ -5,4 +5,39 @@
 //  Created by Bhumika Patel on 21/11/22.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+
+class UserViewModel{
+    weak var vc: SecondApiViewController?
+    var arrUser = [UModel]()
+    
+    func getAllUserDataAF(){
+        AF.request("https://jsonplaceholder.typicode.com/todos/")
+            .response { response in
+                if let data = response.data {
+                do{
+                    let userResponse = try JSONDecoder().decode([UModel].self, from: data)
+                    self.arrUser.append(contentsOf: userResponse)
+                    DispatchQueue.main.async {
+                        self.vc?.tblView.reloadData()
+                    }
+                }catch let err{
+                    print(err.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    func getAllUserData(){
+        URLSession.shared.dataTask(with: URL(string: "https://jsonplaceholder.typicode.com/todos/")!) { [self] (data, respose, error) in
+            if error == nil{
+               
+            }else{
+                print(error?.localizedDescription)
+            }
+        }.resume()
+    }
+}
+
+
---
+++ DifferentTypeApiCalling/SecondApi/View/SecondApiViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondApiViewController.swift
+//  DifferentTypeApiCalling
+//
+//  Created by Bhumika Patel on 21/11/22.
+//
+
+import UIKit
+
+class SecondApiViewController: UIViewController {
+
+    var viewModelUser = UserViewModel()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
